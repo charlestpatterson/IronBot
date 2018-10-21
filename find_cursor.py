3ffd2e52169fbcac61db94a1670e4590fb4f165d
Chuck <n1200123a@yahoo.com>
2018-10-20 20:18:39 -0500

    first push

--- find_cursor.py
+++ find_cursor.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     print('Press Ctrl-C to quit.')
     try:
         while True:
-            # Get and print the mouse coordinates.
+            # Get and print the mouse coordinates
             x, y = pyautogui.position()
             positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
             print("\r", positionStr, end="", flush=True)
